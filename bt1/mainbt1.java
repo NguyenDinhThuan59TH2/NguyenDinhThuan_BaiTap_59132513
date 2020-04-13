0be57d648510c3326fbc867026f90c68526336a0
saodoiemden1999 <saodoiemden1999@gmail.com>
2020-04-13 22:07:34 +0700

    bai 1

---
+++ bt1/mainbt1.java
@@ -0,0 +1,10 @@
+package bt1;
+public class mainbt1 {
+    public static void main(String [] args ){
+        NhanVien nhanvien1 = new NhanVien("Nguyen Dinh Thuan",21, "Quang Nam", (double) 2500000, 51);
+        NhanVien nhanvien2 = new NhanVien("Nguyen Van Thuan",21, "Quang Nam", (double) 2500000, 51);
+
+        nhanvien1.getthongtin();
+        nhanvien1.getthongtin();
+    }
+}
